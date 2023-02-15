b0a5d95444c0040ae8a5961dffbae6c2b7b92f26
Carol Chero Paz <cheropazpilar@gmail.com>
2023-02-15 14:10:18 -0500

    ultimos cambios

---
+++ src/test/java/com/example/miprimerproyecto/MiPrimerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miprimerproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
